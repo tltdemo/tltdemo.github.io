acf288dd46614abab073779eff41d443b0f3229f
vivinantony <vivinantony@gmail.com>
2017-08-27 13:38:49 +0530

    phone html

--- craft/4/app/phone.component.ts
+++ craft/4/app/phone.component.ts
@@ -3,32 +3,7 @@ import { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'phone',
-    template: `
-    <div [formGroup]="phoneForm">
-      <div class="form-group col-xs-6">
-        <label>Phone number</label>
-        <input type="text" 
-               class="form-control" 
-               placeholder="Phone" 
-               formControlName="phone" 
-               phonemask 
-               (rawChange)="rawPhone=$event" 
-               [attr.maxlength]="14">
-        <small *ngIf="phoneForm.controls.phone.hasError('required') && phoneForm.controls.phone.touched" class="text-danger">
-            Phone number is required
-        </small>
-        <small *ngIf="(phoneForm.controls.phone.hasError('minlength') || phoneForm.controls.phone.hasError('maxlength')) && phoneForm.controls.phone.touched" class="text-danger">
-            Phone number is to be 10 numbers long
-        </small>
-      </div>
-      <div class="form-group col-xs-6">
-        <label>Phone Type</label>
-        <select class="form-control" formControlName="phoneType" (ngModelChange)="onClick($event)">
-          <option *ngFor="let type of phoneTypes" value="{{type.value}}" title="{{type.desc}}">{{type.name}}</option>
-        </select>
-      </div>
-    </div>
-    `
+     templateUrl: 'phone.component.html',
 })
 export class PhoneComponent {
     @Input('group')
