20c606b8d9f662de043822294bca94980f6e9aa4
vivinantony <vivinantony@gmail.com>
2017-08-30 09:57:40 +0530

    tags added to html

--- js/app.js
+++ js/app.js
@@ -3,5 +3,22 @@ var app = angular.module('app', []);
 app.controller('commonCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('../json/list.json').then(function(res) {
         $scope.list = res.data;
+
+        var allTags = [];
+        res.data.forEach(function(obj){
+            allTags = allTags.concat(obj.tags);
+        });
+
+        var uniqueTag = { };
+        for (var i = 0, j = allTags.length; i < j; i++) {
+            if (uniqueTag[allTags[i]]) {
+                uniqueTag[allTags[i]]++;
+            }
+            else {
+                uniqueTag[allTags[i]] = 1;
+            } 
+        }
+        console.log(uniqueTag);
+        $scope.uniqueTags = uniqueTag;
     });
 }])
